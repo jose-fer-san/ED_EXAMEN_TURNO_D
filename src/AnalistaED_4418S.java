30ab86b6050f95b5ee3b28eb688b3109ef7da131
jose <jose@aa.cas>
2022-01-19 10:13:19 +0100

    código sin javadoc DNI 4418S

---
+++ src/AnalistaED_4418S.java
@@ -0,0 +1,31 @@
+
+import java.util.*;
+// !!!! MODIFICA TU NOMBRE Y TU DNI DONDE TOQUE
+
+public class AnalistaED_4418S {
+
+    static final double FACTOR_HORA_EXTRA = 1.2;
+    private String nombreCompleto, dni;
+    private double salarioBasePorHora = 10.56;
+
+    public AnalistaED_4418S(String nombreCompleto, String dni) {
+        this.nombreCompleto = nombreCompleto;
+        this.dni = dni;
+    }
+
+    public double getSalarioEsteMes(int horasExtra) {
+        double cantidadExtra, salarioFinal;
+        cantidadExtra = horasExtra * this.salarioBasePorHora
+                * FACTOR_HORA_EXTRA;
+        salarioFinal = this.salarioBasePorHora * 40 * 4 + cantidadExtra;
+        salarioFinal = (double) Math.round(salarioFinal * 100d) / 100d;
+        return salarioFinal;
+    }
+
+    // Este método no lo comentaremos con JavaDoc
+    public static void main(String[] args) {
+        AnalistaED_4418S emp = new AnalistaED_4418S("JOSÉ FERNÁNDEZ","4418S");
+        int horasExtra = 2;
+        System.out.println("El salario de " + emp.nombreCompleto + " este mes será de " + emp.getSalarioEsteMes(horasExtra) + " euros, al hacer " + horasExtra + " horas extra.");
+    }
+}
