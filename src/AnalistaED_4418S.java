64747290411ce2ae19ae4afab67306ec495d912f
jose <jose@aa.cas>
2022-01-19 10:20:22 +0100

    versión final con javadoc DNI 4418S

--- src/AnalistaED_4418S.java
+++ src/AnalistaED_4418S.java
@@ -1,6 +1,9 @@
-
+/**
+ * EJERCICIO PRÁCTICO EXAMEN EVAL 1 ED
+ * @author JOSÉ FERNÁNDEZ DNI 4418S
+ * @version 1.0
+ */
 import java.util.*;
-// !!!! MODIFICA TU NOMBRE Y TU DNI DONDE TOQUE
 
 public class AnalistaED_4418S {
 
@@ -8,11 +11,22 @@ public class AnalistaED_4418S {
     private String nombreCompleto, dni;
     private double salarioBasePorHora = 10.56;
 
+    /**
+     * Constructor con dos parámetros
+     * @param nombreCompleto nombre completo en mayúsculas
+     * @param dni documento nacional de identidad con letra y sin espacios
+     */
     public AnalistaED_4418S(String nombreCompleto, String dni) {
         this.nombreCompleto = nombreCompleto;
         this.dni = dni;
     }
-
+    
+    /**
+     * Calcula el salario bruto mensual en función del salario base por hora y las horas extras.
+     * El precio por hora de la hora extra se determina con la constante FACTOR_HORA_EXTRA
+     * @param horasExtra número de horas extra dedicadas redondeado a la baja (entero)
+     * @return devuelve el salario bruto mensual en euros con dos decimales
+     */
     public double getSalarioEsteMes(int horasExtra) {
         double cantidadExtra, salarioFinal;
         cantidadExtra = horasExtra * this.salarioBasePorHora
